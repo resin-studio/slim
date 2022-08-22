ad8b952883a935697f95e934c38b9a8631b5000d
Thomas Logan <thomas.logan.atx@gmail.com>
2022-08-22 18:03:57 -0500

    in progress

--- sketch.lean
+++ sketch.lean
@@ -1,39 +1,52 @@
 /-
 -- slim logic sketch --
 
--- symbol --
-s ::= [a-zA-Zα-ζ]+
+-- label --
+l ::= [a-z_]+
 
+-- identifier --
+x ::= [a-zA-Zα-ζ][a-zA-Zα-ζ0-9_]*
 
 -- term --
 t ::=
-  $s                           -- unit variant
-  #s t                         -- variant
-  match t (case #s t => t ...) -- variant elimination
-  .s t, ...                    -- record
-  t.s                          -- record elimination
+  _                            -- irrelevant pattern / inferred expression
+  t : τ                        -- typed pattern
+  x                            -- variable expression / pattern
+  $l                           -- unit variant expression / pattern
+  #l t                         -- variant expression / pattern
+  match t (case #l t => t ...) -- variant elimination
+  .l t, ...                    -- record expression / pattern
+  t.l                          -- record elimination 
   t => t                       -- function abstraction
   t t                          -- function application
+  let t = t in t               -- binding
   fix t                        -- recursion
+  τ                            -- type as a term of type * and sort **
 
 -- term sugar --
-(t , t) = (.left t , .right t)
+⟦(t1 , t2)⟧  ~> (.left ⟦t1⟧, .right ⟦t2⟧)
+
+-- term notes --
+we collapse the notion of type with term to be consistent with Python 
 
 -- type --
-T ::=
-  s             -- symbol type
-  ?s     -- unit variant type
-  #s : T     -- variant type
-  .s : T     -- record type
-  T & T        -- intersection type
-  T | T        -- union type
-  T -> T        -- implication type
-  ∀ s . T      -- universal type
-  ∃ s . T      -- existential type
-  μ s . T       -- inductive type
-  { t | t : T } -- relational type
+τ ::=
+  x             -- variable type
+  ?l            -- unit variant type
+  #l : τ        -- variant type
+  .l : τ        -- record type
+  τ & τ         -- intersection type
+  τ | τ         -- union type
+  τ -> τ        -- implication type
+  ∀ x . τ       -- universal type of sort ** with type param of any kind of universe 1
+  ∃ x . τ       -- existential type of sort ** with type param of any kind of universe 1
+  μ x . τ       -- inductive type of kind * with type param of kind *
+  { t | t : τ } -- relational type
+  L             -- kind as type of kind **
 
-Notes: 
+
+
+-- type notes --
 - universal and existential types quantify over types of kind *, resulting in types of kind **
 - these type quantifiers are primitive in this weak logic
 - in a stronger dependently typed / higher kinded logic, these types would be subsumed by implication 
@@ -43,31 +56,33 @@ A /\ B = .left:A & .right:B -- product
 A \/ B = #left:A | #right:B -- sum
 
 
--- kind --
-K ::=
-  *      -- kind of ground types
-  * -> K -- kind of type constructors
+-- lower kind --
+L ::=
+  *         -- kind of ground terms, type of types 
+  * -> L    -- kind of type constructors
 
--- higher kind --
-H ::= **
+-- kind --
+K ::=  
+  L       -- lower kind 
+  **      -- higher kind 
 
 
 -- context --
 Γ ::= 
 
 -- examples --
-def list α = μ list_α . ?nil | #cons:(α /\ list_α)
+let list = α : * => μ list α . ?nil | #cons:(α /\ list α)
 
-def nat = μ nat . ?zero | #succ:nat
+let nat = μ nat . ?zero | #succ:nat
 
-def list_len α = μ list_lenα . (?nil /\ ?zero) | {(#cons (_ : α, xs), #succ n) | (xs, n) : list_len_α}
+let list_len = α : * => μ list_len . (?nil /\ ?zero) | {(#cons (_ : α, xs), #succ n) | (xs, n) : list_len α}
 Note: clauses based on typing membership in relational type
-- obviate the need for SMT solving, 
+- obviate the need for outsourcing to SMT solver, 
 - allow reusing definitions for both checking and refinement
 
-def 4 = #succ (#succ (#succ (#succ $zero)))
+let 4 = #succ (#succ (#succ (#succ $zero)))
 
-def list_4 = {xs | (xs, 4) : list_len nat}
+let list_4 = {xs | (xs, 4) : list_len nat}
 
 %check 1 :: 2 :: 3 :: 4 :: $nil : list_4
 
