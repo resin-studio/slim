ebf11ce1b3d644e19f82908aef35c3a5c7a0af5a
Thomas Logan <thomas.logan.atx@gmail.com>
2022-08-22 13:21:20 -0500

    in progress

--- sketch.lean
+++ sketch.lean
@@ -2,7 +2,7 @@
 -- slim logic sketch --
 
 -- symbol --
-s ::= [a-zA-Z]+
+s ::= [a-zA-Zα-ζ]+
 
 
 -- term --
@@ -56,20 +56,20 @@ H ::= **
 Γ ::= 
 
 -- examples --
-def List T = ?nil | #cons:(T /\ List T)
+def list α = μ list_α . ?nil | #cons:(α /\ list_α)
 
-def Nat = μ Nat . ?zero | #succ:Nat
+def nat = μ nat . ?zero | #succ:nat
 
-def ListLen T = (?nil /\ ?zero) | {(#cons (_ : T, xs), #succ n) | (xs, n) : ListLen T}
+def list_len α = μ list_lenα . (?nil /\ ?zero) | {(#cons (_ : α, xs), #succ n) | (xs, n) : list_len_α}
 Note: clauses based on typing membership in relational type
 - obviate the need for SMT solving, 
 - allow reusing definitions for both checking and refinement
 
 def 4 = #succ (#succ (#succ (#succ $zero)))
 
-def List4 = {xs | (xs, 4) : ListLen Nat}
+def list_4 = {xs | (xs, 4) : list_len nat}
 
-%check 1 :: 2 :: 3 :: 4 :: $nil
+%check 1 :: 2 :: 3 :: 4 :: $nil : list_4
 
 
 
