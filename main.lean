f29b670531cb8bcb2447316c7c9ed17a86fb873e
Thomas Logan <thomas.logan.atx@gmail.com>
2022-09-24 12:12:31 -0500

    update

---
+++ main.lean
@@ -0,0 +1,49 @@
+-- surface syntax 
+/-
+-/
+
+-- concrete canonical syntax 
+/-
+-/
+
+-- abstract canonical syntax 
+/-
+-/
+
+-- dynamic semantics 
+/-
+-/
+
+-- static semantics 
+/-
+-/
+
+
+-- dynamic implementation 
+/-
+beyond scope
+-/
+
+-- static implementation 
+/-
+-/
+
+
+-- dynamic semantics/implementation theorems
+/-
+soundness: beyond scope 
+completeness: beyond scope
+-/
+
+-- static/dynamic semantics theorems
+/-
+soundness: N/A
+completeness: N/A
+-/
+
+
+-- static implementation/semantics theorems
+/-
+soundness: ...
+completeness: ...
+-/
