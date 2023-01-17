0317b357a785959bd1f778c8b8f9243d2f40ca26
Thomas Logan <thomas.logan.atx@gmail.com>
2023-01-17 12:40:35 -0600

    update

--- theory.lean
+++ theory.lean
@@ -1127,21 +1127,6 @@ match t with
   )))
 
 
--- | .letb ty1 t1 t => 
---   List.bind (infer i (env_ty) env_tm aim t1 ty1) (fun (i, env_ty1, ty1') =>
-
---   let ty_u := [: ∀ 1 :: β[0] ≤ ⟨ty1'⟩ => β[0] :]
-
---   let ty1' := (Ty.subst (env_ty;env_ty1) ty1')
---   -- let ty1' := (Ty.simplify (Ty.subst (env_ty;env_ty1) ty1'))
---   let fvs := (Ty.free_vars ty1').toList.reverse.bind (fun (k, _) => [k])
---   let ty1' := if fvs.isEmpty then ty1' else [: ∀ ⟨fvs.length⟩ => ⟨Ty.generalize fvs 0 ty1'⟩ :]
---   let (i, x, env_tmx) := (i + 1, Tm.fvar i, PHashMap.from_list [(i, ty1')]) 
---   let t := Tm.instantiate 0 [x] t 
---   List.bind (infer i (env_ty;env_ty1) (env_tm ; env_tmx) aim t ty) (fun (i, env_ty2, ty') =>
---     [ (i, env_ty2, ty') ]
---   ))
-
 | .letb ty1 t1 t => 
   List.bind (infer i (env_ty) env_tm aim t1 ty1) (fun (i, env_ty1, ty1') =>
   let ty1' := (Ty.subst (env_ty;env_ty1) ty1')
