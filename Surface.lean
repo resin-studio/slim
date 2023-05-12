b09d2c712835bedafe01b76cdbe4550f27eddc3a
Thomas Logan <thomas.logan.atx@gmail.com>
2023-05-12 11:44:34 -0500

    update surface concrete syntax

--- Surface.lean
+++ Surface.lean
@@ -83,23 +83,21 @@ namespace Surface
     | .bot => "⊥" 
     | .top => "⊤" 
     | .tag l ty1 => 
-      (l ++ "*" ++ (Ty.repr ty1 n))
+      (l ++ "@" ++ (Ty.repr ty1 n))
     | .field l ty1 => 
       Format.bracket "(" (l ++ " : " ++ (Ty.repr ty1 n)) ")"
 
-    | .union (Ty.tag "inl" inl) (Ty.tag "inr" inr) =>
-      Format.bracket "(" ((Ty.repr inl n) ++ " +" ++ Format.line ++ (Ty.repr inr n)) ")"
     | .union ty1 ty2 =>
       let _ : ToFormat Ty := ⟨fun ty' => Ty.repr ty' n ⟩
       let tys := [ty1, ty2] 
       Format.bracket "("
-        (Format.joinSep tys (" ∨" ++ Format.line))
+        (Format.joinSep tys (" |" ++ Format.line))
       ")"
 
     | .inter (Ty.field "l" l) (Ty.field "r" r) =>
-      Format.bracket "(" ((Ty.repr l n) ++ " × " ++ (Ty.repr r n)) ")"
+      Format.bracket "(" ((Ty.repr l n) ++ " * " ++ (Ty.repr r n)) ")"
     | .inter ty1 ty2 =>
-      Format.bracket "(" ((Ty.repr ty1 n) ++ " ∧ " ++ (Ty.repr ty2 n)) ")"
+      Format.bracket "(" ((Ty.repr ty1 n) ++ " & " ++ (Ty.repr ty2 n)) ")"
     | .case ty1 ty2 =>
       Format.bracket "(" ((Ty.repr ty1 n) ++ " ->" ++ Format.line ++ (Ty.repr ty2 n)) ")"
     | .univ names ty_c1 ty_c2 ty_pl =>
@@ -111,7 +109,7 @@ namespace Surface
       else
         Format.bracket "(" (
           "forall " ++ names_format ++ " " ++ (Ty.repr ty_c1 n) ++ " <: " ++ (Ty.repr ty_c2 n) ++
-            " . " ++ (Ty.repr ty_pl n)
+            " have " ++ (Ty.repr ty_pl n)
         ) ")"
     | .exis names ty_c1 ty_c2 ty_pl =>
       let names_format := Format.bracket "[" (Format.joinSep names ", ") "]"
@@ -122,8 +120,8 @@ namespace Surface
       else
         Format.bracket "{" (
           names_format ++ " " ++ 
-          (Ty.repr ty_pl n) ++ " | " ++
-          (Ty.repr ty_c1 n) ++ " ≤ " ++ (Ty.repr ty_c2 n)
+          (Ty.repr ty_pl n) ++ " with " ++
+          (Ty.repr ty_c1 n) ++ " <: " ++ (Ty.repr ty_c2 n)
         ) "}"
     | .recur name ty1 =>
       Format.bracket "(" (
@@ -144,25 +142,24 @@ namespace Surface
     syntax:90 "unit" : surftype
     syntax:90 "⊥" : surftype
     syntax:90 "⊤" : surftype
-    syntax:90 surftype:100 "*" surftype:90 : surftype
+    syntax:90 surftype:100 "@" surftype:90 : surftype
     syntax:90 surftype:100 ":" surftype:90 : surftype
     syntax:50 surftype:51 "->" surftype:50 : surftype
-    syntax:60 surftype:61 "∨" surftype:60 : surftype
-    syntax:60 surftype:61 "+" surftype:60 : surftype
-    syntax:70 surftype:71 "∧" surftype:70 : surftype
-    syntax:70 surftype:71 "×" surftype:70 : surftype
+    syntax:60 surftype:61 "|" surftype:60 : surftype
+    syntax:70 surftype:71 "&" surftype:70 : surftype
+    syntax:70 surftype:71 "*" surftype:70 : surftype
 
-    syntax "{" surftype surftype:41 "|" surftype:41 "<:" surftype:41 "}": surftype 
+    syntax "{" surftype surftype:41 "with" surftype:41 "<:" surftype:41 "}": surftype 
     syntax "{" surftype surftype:41 "}" : surftype 
 
-    syntax "{" surftype:41 "|" surftype:41 "<:" surftype:41 "}": surftype 
+    syntax "{" surftype:41 "with" surftype:41 "<:" surftype:41 "}": surftype 
     syntax "{" surftype:41 "}" : surftype 
 
 
-    syntax "forall" surftype surftype:41 "<:" surftype:41 "." surftype:41 : surftype 
+    syntax "forall" surftype surftype:41 "<:" surftype:41 "have" surftype:41 : surftype 
     syntax "forall" surftype surftype:41 : surftype 
 
-    syntax "forall" surftype:41 "<:" surftype:41 "." surftype:41 : surftype 
+    syntax "forall" surftype:41 "<:" surftype:41 "have" surftype:41 : surftype 
     syntax "forall" surftype:41 : surftype 
 
 
@@ -190,28 +187,27 @@ namespace Surface
     | `([surftype: unit ]) => `(Ty.unit)
     | `([surftype: ⊥ ]) => `(Ty.bot)
     | `([surftype: ⊤ ]) => `(Ty.top)
-    | `([surftype: $a * $b:surftype ]) => `(Ty.tag (idname [surftype: $a ]) [surftype: $b ])
+    | `([surftype: $a @ $b:surftype ]) => `(Ty.tag (idname [surftype: $a ]) [surftype: $b ])
     | `([surftype: $a : $b:surftype ]) => `(Ty.field (idname [surftype: $a ]) [surftype: $b ])
     | `([surftype: $a -> $b ]) => `(Ty.case [surftype: $a ] [surftype: $b ])
-    | `([surftype: $a ∨ $b ]) => `(Ty.union [surftype: $a ] [surftype: $b ])
-    | `([surftype: $a + $b ]) => `(Ty.union (Ty.tag "inl" [surftype: $a ]) (Ty.tag "inr" [surftype: $b ]))
-    | `([surftype: $a ∧ $b ]) => `(Ty.inter [surftype: $a ] [surftype: $b ])
-    | `([surftype: $a × $b ]) => `(Ty.inter (Ty.field "l" [surftype: $a ]) (Ty.field "r" [surftype: $b ]))
+    | `([surftype: $a | $b ]) => `(Ty.union [surftype: $a ] [surftype: $b ])
+    | `([surftype: $a & $b ]) => `(Ty.inter [surftype: $a ] [surftype: $b ])
+    | `([surftype: $a * $b ]) => `(Ty.inter (Ty.field "l" [surftype: $a ]) (Ty.field "r" [surftype: $b ]))
 
-    | `([surftype: { $n $d | $b <: $c }  ]) => `(Ty.exis [surftype: $n ] [surftype: $b ] [surftype: $c ] [surftype: $d ])
+    | `([surftype: { $n $d with $b <: $c }  ]) => `(Ty.exis [surftype: $n ] [surftype: $b ] [surftype: $c ] [surftype: $d ])
     | `([surftype: { $n $b:surftype } ]) => `(Ty.exis [surftype: $n ] Ty.unit Ty.unit [surftype: $b ] )
 
-    | `([surftype: { $d | $b <: $c }  ]) => 
+    | `([surftype: { $d with $b <: $c }  ]) => 
       `(Ty.exis (to_list (Ty.infer_abstraction {} [surftype: $d ])) [surftype: $b ] [surftype: $c ] [surftype: $d ])
     | `([surftype: { $b:surftype } ]) => 
       `(Ty.exis (to_list (Ty.infer_abstraction {} [surftype: $b ])) Ty.unit Ty.unit [surftype: $b ] )
 
-    | `([surftype: forall $b <: $c . $d  ]) => 
+    | `([surftype: forall $b <: $c have $d  ]) => 
       `(Ty.univ (to_list (Ty.infer_abstraction {} [surftype: $d ])) [surftype: $b ] [surftype: $c ] [surftype: $d ])
     | `([surftype: forall $b:surftype  ]) => 
       `(Ty.univ (to_list (Ty.infer_abstraction {} [surftype: $b ])) Ty.unit Ty.unit [surftype: $b ] )
 
-    | `([surftype: forall $n $b <: $c . $d ]) => `(Ty.univ [surftype: $n ] [surftype: $b ] [surftype: $c ] [surftype: $d ])
+    | `([surftype: forall $n $b <: $c have $d ]) => `(Ty.univ [surftype: $n ] [surftype: $b ] [surftype: $c ] [surftype: $d ])
     | `([surftype: forall $n $b:surftype ]) => `(Ty.univ [surftype: $n ] Ty.unit Ty.unit [surftype: $b ] )
 
     | `([surftype: induct [ $name ] $a ]) => `(Ty.recur $(Lean.quote (toString name.getId)) [surftype: $a ])
@@ -219,26 +215,26 @@ namespace Surface
     #check [surftype: (x) ]
     #check [surftype: [x] ]
     #eval Ty.infer_abstraction {} [surftype: thing ]
-    #eval [surftype: {thing ∨ unit | thing <: unit }]
+    #eval [surftype: {thing | unit with thing <: unit }]
 
 
-    #eval [surftype: forall bob -> {thing ∨ unit | thing <: bob }]
-    #eval [surftype: forall thing -> {thing ∨ bob ∨ unit | thing <: unit }]
+    #eval [surftype: forall bob -> {thing | unit with thing <: bob }]
+    #eval [surftype: forall thing -> {thing | bob | unit with thing <: unit }]
     -- #eval [normam: forall β[0] -> {β[1] ∨ β[0] ∨ unit | β[1] <: unit }]
 
-    #eval [surftype: {[thing, or] thing ∨ unit | thing <: unit }]
-    #eval [surftype: {thing ∨ unit | thing <: unit }]
+    #eval [surftype: {[thing, or] thing | unit with thing <: unit }]
+    #eval [surftype: {thing | unit with thing <: unit }]
     -- #eval Ty.infer_abstraction 0 [surftype: $d ]) [surftype: $b ] [surftype: $c ] [surftype: $d ]
     #eval [surftype: succ*x ]
     #eval [surftype: 
-        (zero*unit × nil*unit) ∨ 
-        {succ*nat × cons*list | nat × list <: nat_list}
+        (zero@unit * nil@unit) |
+        {succ@nat * cons@list with nat * list <: nat_list}
     ]
 
     #eval [surftype: 
       induct [nat_list] (
-        (zero*unit × nil*unit) ∨ 
-        {succ*nat × cons*list | nat × list <: nat_list}
+        (zero@unit * nil@unit) | 
+        {succ@nat * cons@list with nat * list <: nat_list}
       )
     ]
 
@@ -307,13 +303,11 @@ namespace Surface
       let (stack2, ty2') <- (normalize bound_vars ty2) 
       some (stack1 ++ stack2, .case ty1' ty2')
     | .exis names ty1 ty2 ty3 => do
-      let names <- pattern_abstraction ty3
       let (stack1, ty1') <- (normalize (names ++ bound_vars) ty1) 
       let (stack2, ty2') <- (normalize (names ++ bound_vars) ty2) 
       let (stack3, ty3') <- (normalize (names ++ bound_vars) ty3) 
       some (names :: stack1 ++ stack2 ++ stack3, .exis names.length ty1' ty2' ty3')
     | .univ names ty1 ty2 ty3 => do
-      let names <- pattern_abstraction ty3
       let (stack1, ty1') <- (normalize (names ++ bound_vars) ty1) 
       let (stack2, ty2') <- (normalize (names ++ bound_vars) ty2) 
       let (stack3, ty3') <- (normalize (names ++ bound_vars) ty3) 
@@ -417,26 +411,24 @@ namespace Surface
       Format.bracket "(" (Format.joinSep [l, r] ("," ++ Format.line)) ")"
     | record fds =>
       let _ : ToFormat (String × Tm) := ⟨fun (l, t1) =>
-        l ++ " := " ++ Tm.repr t1 n ⟩
-      "σ" ++ Format.bracket "[" (Format.joinSep fds ("," ++ Format.line)) "]"
-    | func [(pat, tb)] =>
-      "λ " ++ (Tm.repr pat n) ++ " => " ++ (Tm.repr tb (n))
+        "#" ++ l ++ " = " ++ Tm.repr t1 n ⟩
+      Format.bracket "(" (Format.joinSep fds (Format.line)) ")"
     | func fs =>
       let _ : ToFormat (Tm × Tm) := ⟨fun (pat, tb) =>
-        "for " ++ (Tm.repr pat n) ++ " => " ++ (Tm.repr tb (n))
+        "\\ " ++ (Tm.repr pat n) ++ " => " ++ (Tm.repr tb (n))
       ⟩
-      "λ" ++ Format.bracket "[" (Format.joinSep fs ("," ++ Format.line)) "]"
+      Format.bracket "(" (Format.joinSep fs (Format.line)) ")"
     | .proj t1 l =>
-      Tm.repr t1 n ++ "/" ++ l
+      Tm.repr t1 n ++ "." ++ l
     | .app t1 t2 =>
       Format.bracket "(" (Tm.repr t1 n) ") " ++ "(" ++ Tm.repr t2 n ++ ")"
     | .letb name op_ty1 t1 t2 =>
       match op_ty1 with
       | some ty1 =>
-        "let " ++ name ++ " : " ++ (Ty.repr ty1 n) ++ " = " ++  (Tm.repr t1 n) ++ " =>" ++
+        "let " ++ name ++ " : " ++ (Ty.repr ty1 n) ++ " = " ++  (Tm.repr t1 n) ++ " in" ++
         Format.line  ++ (Tm.repr t2 n) 
       | none =>
-        "let " ++ name ++ " = " ++  (Tm.repr t1 n) ++ " =>" ++
+        "let " ++ name ++ " = " ++  (Tm.repr t1 n) ++ " in" ++
         Format.line  ++ (Tm.repr t2 n) 
     | .fix t1 =>
       Format.bracket "(" ("fix " ++ (Tm.repr t1 n)) ")"
@@ -457,16 +449,22 @@ namespace Surface
     syntax:30 "_" : surfterm
     syntax:30 "()" : surfterm
     syntax:30 surfterm:100 ";" surfterm:30 : surfterm
-    syntax:30 surfterm:100 ":=" surfterm:30 : surfterm
     syntax:30 "(" surfterm "," surfterm ")" : surfterm
-    syntax:30 "σ" surfterm : surfterm
-    syntax:20 surfterm:30 "=>" surfterm:20 : surfterm 
-    syntax:20 "λ" surfterm:30 "=>" surfterm:20 : surfterm 
-    syntax:30 "λ" surfterm : surfterm 
+
+    -- syntax:20 surfterm:30 "=>" surfterm:20 : surfterm 
+    -- syntax:20 "λ" surfterm:30 "=>" surfterm:20 : surfterm 
+    -- syntax:30 "λ" surfterm : surfterm 
+
+    syntax:30 "#" ident "=" surfterm:30 : surfterm
+    syntax:30 "#" ident "=" surfterm:30 surfterm: surfterm
+
+    syntax:20 "\\" surfterm:30 "=>" surfterm:20 : surfterm
+    syntax:20 "\\" surfterm:30 "=>" surfterm:20 surfterm: surfterm
+
     syntax:30 surfterm:30 "." surfterm:100 : surfterm 
     syntax:30 "(" surfterm:30 surfterm:30 ")" : surfterm 
-    syntax:30 "let" surfterm ":" surfterm:30 "=" surfterm:30 "=>" surfterm:30 : surfterm 
-    syntax:30 "let" surfterm "=" surfterm:30 "=>" surfterm:30 : surfterm 
+    syntax:30 "let" ident ":" surfterm:30 "=" surfterm:30 "in" surfterm:30 : surfterm 
+    syntax:30 "let" ident "=" surfterm:30 "in" surfterm:30 : surfterm 
     syntax:30 "fix " surfterm:30 : surfterm 
 
     syntax "[surfterm: " surfterm "]" : term
@@ -474,6 +472,15 @@ namespace Surface
     def idname v := match v with 
     | (Tm.id x) => x 
     | _ => ""
+
+    def record_fields : Tm -> List (String × Tm)
+    | record fields => fields
+    | _ =>  []
+
+    def function_cases : Tm -> List (Tm × Tm)
+    | func cases => cases 
+    | _ =>  []
+
 
     macro_rules
     --generic
@@ -490,16 +497,18 @@ namespace Surface
     | `([surfterm: _ ]) => `(Tm.hole)
     | `([surfterm: () ]) => `(Tm.unit)
     | `([surfterm: $a ; $b ]) => `(Tm.tag (idname [surfterm: $a ]) [surfterm: $b ])
-    | `([surfterm: $a := $b ]) => `((idname [surfterm: $a ], [surfterm: $b ]))
-    | `([surfterm: $b => $d ]) => `(([surfterm: $b ], [surfterm: $d ]))
-    | `([surfterm: σ $a ]) => `(Tm.record [surfterm: $a ])
     | `([surfterm: ( $a , $b ) ]) => `(Tm.record [("l", [surfterm: $a ]), ("r", [surfterm:$b ])])
-    | `([surfterm: λ $b => $d ]) => `(Tm.func [([surfterm: $b ], [surfterm: $d ])])
-    | `([surfterm: λ $a ]) => `(Tm.func [surfterm: $a ])
+
+    | `([surfterm: # $a = $b ]) => `( Tm.record [ ($(Lean.quote (toString a.getId)), [surfterm: $b ]) ]  )
+    | `([surfterm: # $a = $b $xs ]) => `( Tm.record (($(Lean.quote (toString a.getId)), [surfterm: $b ]) :: (Tm.record_fields [surfterm: $xs ])))
+
+    | `([surfterm: \ $b => $d ]) => `(Tm.func [([surfterm: $b ], [surfterm: $d ])])
+    | `([surfterm: \ $b => $d $xs ]) => `( Tm.func (([surfterm: $b ], [surfterm: $d ]) :: (Tm.function_cases [surfterm: $xs ])))
+
     | `([surfterm: $a . $b ]) => `(Tm.proj [surfterm: $a ] [surfterm: $b ])
     | `([surfterm: ($a $b) ]) => `(Tm.app [surfterm: $a ] [surfterm: $b ])
-    | `([surfterm: let $name : $a = $b => $c ]) => `(Tm.letb [surfterm: $name ] (Option.some [surfterm: $a ]) [surfterm: $b ] [surfterm: $c ])
-    | `([surfterm: let $name = $b => $c ]) => `(Tm.letb [surfterm: $name ] Option.none [surfterm: $b ] [surfterm: $c ])
+    | `([surfterm: let $name : $a = $b in $c ]) => `(Tm.letb $(Lean.quote (toString name.getId)) (Option.some [surfterm: $a ]) [surfterm: $b ] [surfterm: $c ])
+    | `([surfterm: let $name = $b in $c ]) => `(Tm.letb $(Lean.quote (toString name.getId)) Option.none [surfterm: $b ] [surfterm: $c ])
     | `([surfterm: fix $a ]) => `(Tm.fix [surfterm: $a ])
 
 
@@ -512,19 +521,19 @@ namespace Surface
     ]
 
     #eval [surfterm:
-      fix(λ self => λ[
-        (succ;x, succ;y) => (self (x, y)),
-        (zero;(), y) => y,
-        (x, zero;()) => x 
-      ])
+      fix(\ self => (
+        \ (succ;x, succ;y) => (self (x, y))
+        \ (zero;(), y) => y
+        \ (x, zero;()) => x 
+      ))
     ]
 
     #eval [surfterm:
-      λ [x => x]
+      \ x => x
     ]
 
     #eval [surfterm:
-      λ [x => ⟨Tm.id "x"⟩]
+      \ x => ⟨Tm.id "x"⟩
     ]
     end Tm
 
@@ -667,16 +676,37 @@ namespace Surface
 
   def nat_list := [surftype: 
     induct [nat_list] (
-      (zero*unit × nil*unit) ∨ 
-      {succ*nat × cons*list | nat × list <: nat_list}
+      (zero@unit * nil@unit) | 
+      {succ@nat * cons@list with nat * list <: nat_list}
     )
   ]
   #eval nat_list
 
   def nat_to_list := [surftype:
-    forall nat -> {list | nat × list <: nat_list}
+    forall nat -> {list with nat * list <: nat_list}
   ] 
 
   #eval nat_to_list
+
+  #eval  [surfterm:
+    fix(\ self => ( 
+      \ (succ;x, succ;y) => (self (x, y))
+      \ (zero;(), y) => y
+      \ (x zero;()) => x 
+    )) 
+  ]
+  
+  #eval  [surfterm:
+    let f = fix(\ self => ( 
+      \ (succ;x, succ;y) => (self (x, y))
+      \ (zero;(), y) => y
+      \ (x zero;()) => x 
+    )) in 
+    (f (succ;succ;zero;(), succ;zero;()))
+  ]
+
+  ----------------------------------
+  #eval [surfterm: #x = hello;() #y = world;()]
+  --------------------------------------
 
 end Surface
